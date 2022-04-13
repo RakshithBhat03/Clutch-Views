e4d04857d9c834607185b7be3ef3a86e4318a0f3
RakshithBhat03 <rakshith6666@gmail.com>
2022-04-13 10:29:17 +0530

    feat: added iamge overlay component

---
+++ src/components/ImageOverlay.jsx
@@ -0,0 +1,10 @@
+import "./ImageOverlay.css";
+function ImageOverlay() {
+  return (
+    <div className="overlay-container">
+      <img src="./assets/hero.webp" alt="Hero" />
+    </div>
+  );
+}
+
+export { ImageOverlay };
