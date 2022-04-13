9034b6ca3c7f248733f11a4708a3be8b68417765
RakshithBhat03 <rakshith6666@gmail.com>
2022-04-13 16:34:07 +0530

    feat: added time component

---
+++ src/components/Time/Time.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import { getFormattedTime } from "../../utils/getFormattedTime";
+import "./Time.css";
+function Time() {
+  const [date, setDate] = useState(new Date());
+  const [isTime24Formart, setTimeIs24Format] = useState(false);
+  const { time12Format, time24Format } = getFormattedTime(date);
+  useEffect(() => {
+    setInterval(() => setDate(new Date()), 1000);
+  }, []);
+  return (
+    <div className="time-wrapper">
+      <p className="display-time">
+        {isTime24Formart ? time24Format : time12Format}
+      </p>
+      <button
+        className="btn-time-switch"
+        onClick={() => setTimeIs24Format((state) => !state)}>
+        <i class="fas fa-sync"></i>
+      </button>
+    </div>
+  );
+}
+
+export { Time };
