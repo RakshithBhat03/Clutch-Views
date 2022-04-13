3f1d5041b68dc46e234dadb31de2315d0435d468
RakshithBhat03 <rakshith6666@gmail.com>
2022-04-13 16:34:24 +0530

    feat: added home page component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { ImageOverlay } from "./components/ImageOverlay";
 import "./App.css";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
     <>
       <ImageOverlay />
+      <Home />
     </>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { Time } from "../components/Time/Time";
+import "./Home.css";
+function Home() {
+  return (
+    <div className="home-wrapper">
+      <Time />
+    </div>
+  );
+}
+
+export { Home };
