b4906bb259c88311e5acfa8a8363dee08452fb26
RakshithBhat03 <rakshith6666@gmail.com>
2022-04-14 13:13:44 +0530

    feat: added weather modal toggle

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -3,8 +3,10 @@ import axios from "axios";
 import { APIKEY, DEFAULT_TEMPERATURE } from "../../constants/constants";
 import { setTemperatureData } from "../../utils/setTemperatureData";
 import "./Weather.css";
+import { WeatherModal } from "./WeatherModal";
 function Weather() {
   const [temperature, setTemperature] = useState(DEFAULT_TEMPERATURE);
+  const [showModal, setShowModal] = useState(false);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   useEffect(() => {
@@ -23,22 +25,30 @@ function Weather() {
     })();
   }, []);
   return (
-    <div className="weather-wrapper mr-9 mt-9">
-      {loading && <p>Loading...</p>}
+    <>
+      <div
+        className="weather-wrapper mr-9 mt-9"
+        onClick={() => setShowModal((state) => !state)}>
+        {loading && <p>Loading...</p>}
 
-      {!error && !loading && (
-        <div className="weather-temp-wrapper">
-          <img
-            className="weather-icon"
-            src={`https://openweathermap.org/img/wn/${temperature.icon}.png`}
-            alt="weather"></img>
-          <p className="city-temperature">{`${temperature.temp}°C`}</p>{" "}
-        </div>
+        {!error && !loading && (
+          <div className="weather-temp-wrapper">
+            <img
+              className="weather-icon"
+              src={`https://openweathermap.org/img/wn/${temperature.icon}.png`}
+              alt="weather"
+            />
+            <p className="city-temperature">{`${temperature.temp}°C`}</p>{" "}
+          </div>
+        )}
+        {error && <p>{error}</p>}
+
+        <p className="weather-city">{temperature.city_name}</p>
+      </div>
+      {showModal && (
+        <WeatherModal temperature={temperature} setShowModal={setShowModal} />
       )}
-      {error && <p>{error}</p>}
-
-      <p className="weather-city">{temperature.city_name}</p>
-    </div>
+    </>
   );
 }
 
