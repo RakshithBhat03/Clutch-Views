ef2c2d81beb9629c75722e8b0f1eb190c595e393
RakshithBhat03 <rakshith6666@gmail.com>
2022-04-14 11:12:52 +0530

    chore: export for weather component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { ImageOverlay } from "./Image Overlay/ImageOverlay";
 export { Time } from "./Time/Time";
 export { Quote } from "./Quote/Quote";
+export { Weather } from "./Weather/Weather";
