e8d9fef3d332344edc9c8cc8683b5f8da0907bc2
RakshithBhat03 <rakshith6666@gmail.com>
2022-04-14 11:16:14 +0530

    feat: added constants for weather

---
+++ src/constants/constants.js
@@ -0,0 +1,11 @@
+export const APIKEY = "368f1a023a7320666c673863eb7cd18c";
+export const DEFAULT_TEMPERATURE = {
+  city_name: "",
+  description: "",
+  feels_like: "",
+  humidity: "",
+  icon: "",
+  temp_max: "",
+  temp_min: "",
+  temp: "",
+};
