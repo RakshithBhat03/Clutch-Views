9ef6ab284a88a32ba622e90a96259ec754c0460a
RakshithBhat03 <rakshith6666@gmail.com>
2022-04-14 18:26:43 +0530

    chore: export for greeting

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { ImageOverlay } from "./Image Overlay/ImageOverlay";
 export { Time } from "./Time/Time";
 export { Quote } from "./Quote/Quote";
 export { Weather } from "./Weather/Weather";
+export { Greeting } from "./Greeting/Greeting";
