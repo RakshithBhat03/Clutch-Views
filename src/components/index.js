424e687279e2a7eef8eb44308b07476606740924
RakshithBhat03 <rakshith6666@gmail.com>
2022-04-14 21:16:12 +0530

    chore: export for focus

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { Time } from "./Time/Time";
 export { Quote } from "./Quote/Quote";
 export { Weather } from "./Weather/Weather";
 export { Greeting } from "./Greeting/Greeting";
+export { Focus } from "./Focus/Focus";
