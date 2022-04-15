1d5252a21dc1e34c096940efabda4370cfe55a7f
RakshithBhat03 <rakshith6666@gmail.com>
2022-04-15 19:08:42 +0530

    feat: added user context

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,26 @@
+import { useEffect } from "react";
 import { Time, Quote, Weather, Focus } from "../components/";
+import { Onboarding } from "../components/Onboarding/Onboarding";
+import { UseUser } from "../context/User";
 import "./Home.css";
 function Home() {
+  const { user, dispatch } = UseUser();
+  const { isRegistered } = user;
+  useEffect(() => {
+    dispatch({ type: "GET_USER" });
+  }, []);
   return (
     <div className="home-wrapper">
-      <Time />
-      <Focus />
-      <Quote />
-      <Weather />
+      {!isRegistered ? (
+        <Onboarding />
+      ) : (
+        <>
+          <Time />
+          <Focus />
+          <Quote />
+          <Weather />
+        </>
+      )}
     </div>
   );
 }
