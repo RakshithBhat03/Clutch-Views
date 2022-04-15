54475a44241e10630c951b2e26950b30f56beb01
RakshithBhat03 <rakshith6666@gmail.com>
2022-04-15 19:09:03 +0530

    feat: added user onboarding component

---
+++ src/components/Onboarding/Onboarding.jsx
@@ -0,0 +1,28 @@
+import { UseUser } from "../../context/User";
+import "./Onboarding.css";
+function Onboarding() {
+  const { user, dispatch } = UseUser();
+  const { userName } = user;
+  return (
+    <div className="onboarding display-flex flex-col align-items-center gap-2">
+      <p className="onboarding-text txt-white">Hello, what's your name?</p>
+      <input
+        type="text"
+        className="onboarding-name txt-center pb-5"
+        onChange={(e) =>
+          dispatch({ type: "ENTER_NAME", payload: e.target.value })
+        }
+        value={userName}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            if (userName !== "") {
+              dispatch({ type: "REGISTER" });
+            }
+          }
+        }}
+      />
+    </div>
+  );
+}
+
+export { Onboarding };
