b0a431b6f82a1383516daf25b52fdd661730c059
RakshithBhat03 <rakshith6666@gmail.com>
2022-04-15 19:10:00 +0530

    feat: added dynamic username with user context

--- src/components/Greeting/Greeting.jsx
+++ src/components/Greeting/Greeting.jsx
@@ -1,11 +1,17 @@
 import "./Greeting.css";
+import { UseUser } from "../../context/User";
 function Greeting({ hours }) {
+  const { user } = UseUser();
   const greeting =
     (hours < 12 && `Morning`) ||
     (hours < 17 && `Afternoon`) ||
     hours < 5 ||
     (hours >= 17 && `Evening`);
-  return <p className="greeting mt-9">Good {greeting}, User.</p>;
+  return (
+    <p className="greeting mt-9">
+      Good {greeting}, {user.userName}.
+    </p>
+  );
 }
 
 export { Greeting };
